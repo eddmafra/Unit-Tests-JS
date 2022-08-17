e7006cc4b2f84f42c6b6ad1c64766b73daa9f826
Eddmafra <eduardo_mafra@hotmail.com>
2022-08-17 18:02:21 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,20 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails())).toBeTruthy();
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(Object.keys(productDetails())).toHaveLength(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof Object.keys(productDetails())).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(Object.values(productDetails('x', 'y'))[0]).not.toBe(Object.values(productDetails('x', 'y'))[1]);
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails()[0].details.productId).toContain('123');
+    expect(productDetails()[1].details.productId).toContain('123');
+
   });
 });
